afe78306ed7966db721f78d3bd330191a388046d
BuildTools <unconfigured@null.spigotmc.org>
2015-08-20 13:36:26 -0400

    Initial Commit

---
+++ src/net/sparkzz/shops/Shops.java
@@ -0,0 +1,19 @@
+package net.sparkzz.shops;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Brendon on 8/20/2015.
+ */
+public class Shops extends JavaPlugin {
+
+	@Override
+	public void onDisable() {
+
+	}
+
+	@Override
+	public void onEnable() {
+
+	}
+}
